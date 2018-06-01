aae87608e193ac09b95e4805279b50cd2daca08c
Ricardo Casares <ricardo_casares@epam.com>
2018-06-01 12:27:45 +0200

    Cleanup API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,6 @@
 import { GraphQLClient } from "graphql-request";
-import * as translit from "translitit-cyrillic-russian-to-latin";
 
 import GQL from "./queries";
-import { date } from "../lib/fx";
 import { compose, createError } from "../lib/helpers";
 import { API, TOP, MONTHS, ErrorTypes } from "../lib/constants";
 
@@ -20,14 +18,14 @@ export async function TopSpeakers(n: number) {
 export async function UpcomingEvents() {
   return client
     .request(GQL.upcomingEvents)
-    .then(compose(latin, format, extract))
+    .then(compose(extract))
     .catch(err => {
       throw createError(err.message, ErrorTypes.Rolling);
     });
 }
 
 function top(n: number) {
-  return compose(limit(n), sort, latin, filter, map, extract);
+  return compose(limit(n), sort, filter, map, extract);
 }
 
 function extract(data) {
@@ -51,20 +49,6 @@ function sort(data) {
 
 function limit(n: number) {
   return data => data.slice(0, n);
-}
-
-function format(data) {
-  return data.map(({ name, day, month }) => ({
-    name,
-    date: date(day, month)
-  }));
-}
-
-function latin(data) {
-  return data.map(({ name, ...rest }) => ({
-    ...rest,
-    name: translit(name.replace("&", "and"))
-  }));
 }
 
 export default { TopSpeakers, UpcomingEvents };
