60930dadd5ccc5591d29a7b0819c9001a5233202
Ricardo Casares <ricardo_casares@epam.com>
2018-06-01 15:56:33 +0200

    Add typings

--- src/lib/fx.ts
+++ src/lib/fx.ts
@@ -4,15 +4,15 @@ export function audio(src) {
   return `<audio src="${src}" />`;
 }
 
-export function pause(n) {
+export function pause(n: number = 1): string {
   return `<break time="${n}s"/>`;
 }
 
-export function interject(str) {
+export function interject(str: string): string {
   return `<say-as interpret-as="interjection">${str}</say-as>`;
 }
 
-export function date(day, month) {
+export function date(day: string, month: string): string {
   return `<say-as interpret-as="date" format="dm">${
     MONTHS[month]
   } ${day}</say-as>`;
